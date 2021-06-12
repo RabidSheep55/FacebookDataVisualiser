caa59f391195b05d59daf55eff53484d5c5a5a53
RabidSheep55 <tharreaupierre@gmail.com>
2021-06-12 18:16:36 +0100

    styling changes

--- plotDMsHistory.py
+++ plotDMsHistory.py
@@ -14,7 +14,7 @@ _temp = [[user, data[user]["sentTotal"], data[user]["receivedTotal"]] for user i
 topUsers = np.array(sorted(_temp, key=lambda x: int(x[1])+int(x[2]), reverse=True))[:N, 0]
 
 # Create pallete
-pal = color_palette(palette='pastel', n_colors=N)
+pal = color_palette(palette='rocket', n_colors=N)
 
 # Group into all messages and compute density functions, grabbing the min and max timestamps
 timestampLists = [data[user]["sentTimestamps"] + data[user]["receivedTimestamps"] for user in topUsers]
@@ -29,7 +29,7 @@ for timestamps in timestampLists:
     if min(timestamps) < mini: mini = min(timestamps)
     if max(timestamps) > maxi: maxi = max(timestamps)
 
-x = np.linspace(mini, maxi, 200)
+x = np.linspace(mini-(maxi-mini)/6, maxi, 200)
 
 # Compute traces and normalize
 densities = []
@@ -43,9 +43,10 @@ for i, user in enumerate(topUsers):
 
     plt.fill_between(x, densities[i] - i*0.8, y2=-i*0.8, color=pal[i], label=user, zorder=i)
     plt.plot(x, densities[i] - i*0.8, c='w', zorder=i, linewidth=1)
-    plt.text(np.min(x), -i*0.8 + 0.12, user, c=pal[i])
+    plt.text(np.min(x), -i*0.8 + 0.1, user, c=pal[i])
 
-plt.yticks([])
-plt.axis('off')
-
+plt.box(False)
+plt.tick_params(axis='y', left=False, top=False, right=False, bottom=False, labelleft=False, labeltop=False, labelright=False, labelbottom=False)
+plt.tick_params(axis='x', colors=pal[0])
+plt.tight_layout()
 plt.show()

26f6df8d9128c4e729807a3992a1a4263cd3b7c8
RabidSheep55 <tharreaupierre@gmail.com>
2021-06-12 22:42:46 +0100

    More styling changes

--- plotDMsHistory.py
+++ plotDMsHistory.py
@@ -14,7 +14,8 @@ _temp = [[user, data[user]["sentTotal"], data[user]["receivedTotal"]] for user i
 topUsers = np.array(sorted(_temp, key=lambda x: int(x[1])+int(x[2]), reverse=True))[:N, 0]
 
 # Create pallete
-pal = color_palette(palette='rocket', n_colors=N)
+pal = color_palette(palette='viridis', n_colors=N+2)
+# rocket, viridis, Set2, Paired
 
 # Group into all messages and compute density functions, grabbing the min and max timestamps
 timestampLists = [data[user]["sentTimestamps"] + data[user]["receivedTimestamps"] for user in topUsers]
@@ -22,14 +23,15 @@ densityFuncs = []
 mini, maxi = np.inf, 0
 for timestamps in timestampLists:
     density = gaussian_kde(timestamps)
-    density.covariance_factor = lambda : 0.20
+    density.covariance_factor = lambda : 0.12
     density._compute_covariance()
     densityFuncs += [density]
 
     if min(timestamps) < mini: mini = min(timestamps)
     if max(timestamps) > maxi: maxi = max(timestamps)
 
-x = np.linspace(mini-(maxi-mini)/6, maxi, 200)
+# Create x-space (with extra padding on the left)
+x = np.linspace(mini-(maxi-mini)/7, maxi, 600)
 
 # Compute traces and normalize
 densities = []
@@ -37,16 +39,22 @@ for i in range(N):
     density = densityFuncs[i](x)
     densities += [np.array(density/max(density))]
 
+# Convert x to datetime
 x = (x/1000).astype(np.datetime64)
 
+# Plot
+plt.figure(figsize=(8, N*6/15))
 for i, user in enumerate(topUsers):
-
     plt.fill_between(x, densities[i] - i*0.8, y2=-i*0.8, color=pal[i], label=user, zorder=i)
-    plt.plot(x, densities[i] - i*0.8, c='w', zorder=i, linewidth=1)
+    plt.plot(x, densities[i] - i*0.8 + 0.025, c='w', zorder=i, linewidth=1)
     plt.text(np.min(x), -i*0.8 + 0.1, user, c=pal[i])
 
 plt.box(False)
 plt.tick_params(axis='y', left=False, top=False, right=False, bottom=False, labelleft=False, labeltop=False, labelright=False, labelbottom=False)
-plt.tick_params(axis='x', colors=pal[0])
+plt.tick_params(axis='x', colors=pal[N-1])
+plt.suptitle("Normalized Frequency for Facebook Messages sent over time", c=pal[0], fontsize=12, y=0.96)
+plt.title("[Top trace has the most total messages]", c=pal[0], fontdict={'fontsize': 10}, pad=0.8)
 plt.tight_layout()
+
+plt.savefig(os.path.join("outputs", "DM History Plot.png"), dpi=500)
 plt.show()

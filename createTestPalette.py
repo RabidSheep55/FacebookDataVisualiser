9b53722cb7bc57b4266dbe249465d895cd67d7ab
RabidSheep55 <tharreaupierre@gmail.com>
2021-06-17 15:01:22 +0100

    styling changes

--- createTestPalette.py
+++ createTestPalette.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os, json
 N = 10
 # pal = color_palette("blend:#20ADAB,#636e72", n_colors=N)
-pal = color_palette("blend:#ff7675,#00b0ac", n_colors=N)
+pal = color_palette("blend:#2d3436,#00b0ac", n_colors=N)
 # pal = color_palette("ch:start=.2,rot=-.3", n_colors=N)
 palplot(pal)
 plt.tight_layout()
